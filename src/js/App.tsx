9a739f5875dc73826381cf3846dbd2b5fef143f1
mayu0130 <ktmyk5272@gmail.com>
2025-04-22 13:37:33 +0900

    ルーティング（ReactRouter） Part.1

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -4,15 +4,22 @@ import { useTimer } from "./components/todo/use-timer";
 import { useAuth } from "./contexts/use-auth";
 import { Login } from "./pages/Login";
 import { Todo } from "./pages/Todo";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from "react-router-dom";
+import { create } from "framer-motion/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/login" element={<Login />} />
+      <Route path="/todo" element={<Todo />} />
+    </>
+  ),
+);
 
 
-export const App = () => {
-  const {isLoggedIn} = useAuth();
-
-  if(!isLoggedIn){
-    return< Login />
-  };
-  return<Todo />
-
-
+export const App = () =>{
+  return (
+      <RouterProvider router={router} />
+  );
 };
